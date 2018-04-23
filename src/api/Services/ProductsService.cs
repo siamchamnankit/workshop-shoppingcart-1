9d15cdfd74f7f86bedb957093b44d8df01d2a433
iFew <chitpong@gmail.com>
2018-04-23 16:00:06 +0700

    [Added] แสดงรายการ Product

---
+++ src/api/Services/ProductsService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using api.Models;
+
+namespace api.Services
+{
+    public class ProductsService
+    {
+        internal IEnumerable<ProductsModel> list()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

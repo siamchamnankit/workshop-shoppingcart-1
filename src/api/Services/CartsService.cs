85987e568094711c8c73a4527d74510cfa129f83
iFew <chitpong@gmail.com>
2018-04-24 11:42:47 +0700

    [Edited] เพิ่มการ add cart ใน service

--- src/api/Services/CartsService.cs
+++ src/api/Services/CartsService.cs
@@ -18,5 +18,23 @@ namespace api.Services
         {
              return _context.Carts.ToList();
         }
+
+        internal AddCartOutputModel add(int product_id, int quantity) {
+            CartsModel cartModel = new CartsModel{
+                userId = 1,
+                subtotal = 10,
+                total = 10,
+                shippingMethod = "OK",
+                shippingFee = 50,
+                shippingId = 1,
+                createDatetime = DateTime.Now,
+                updateDatetime = DateTime.Now
+            };
+            _context.Carts.Add(cartModel);
+            _context.SaveChanges();
+            return new AddCartOutputModel{
+                id = cartModel.id
+            };
+        }
     }
 }

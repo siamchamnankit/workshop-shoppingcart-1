d2e290a899255f15e58efde8cb2c5b3f43bfae56
aj.jammy <aj.jammy09@gmail.com>
2018-04-24 11:06:18 +0700

    [Created] สร้างไฟล์ CartsContext สำหรับเชื่อมต่องกับ database

---
+++ src/api/Models/CartsContext.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Models
+{
+    public class CartsContext : DbContext
+    {
+        public CartsContext(DbContextOptions<CartsContext> dbContextOptions) :
+            base(dbContextOptions)
+        {
+        }
+
+        public DbSet<CartsModel> Carts { get; set; }
+        public DbSet<CartProductsModel> CartProducts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CartsModel>()
+                        .HasKey(m => m.id);
+
+            modelBuilder.Entity<CartProductsModel>()
+                        .HasKey(a => a.id);
+
+            modelBuilder.Entity<CartsModel>()
+                        .HasMany(a => a.CartProducts)
+                        .WithOne(m => m.Carts)
+                        .HasForeignKey(a => a.cartId);
+        }
+    }
+}

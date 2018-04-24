885451965dbe8a358c4b60f78d859c9d36a9c0df
nathawat <n.thumthiwong@gmail.com>
2018-04-24 17:28:19 +0700

    [Edited] แก้ให้สามารถสร้าง order ผ่าน api ได้

--- src/ui/assets/js/core.js
+++ src/ui/assets/js/core.js
@@ -2,5 +2,6 @@
 const getFrontendURL = (path = '') => `http://${location.host}/${path}`
 const redirectToIndex = () => location.href = getFrontendURL();
 const redirectToCart = () => location.href = getFrontendURL('cart.html');
+const redirectToOrderSummary = () => location.href = getFrontendURL('order_detail.html');
 //Backend
 const getApiURL = (path = '') => `http://${location.host}:5001/${path}`

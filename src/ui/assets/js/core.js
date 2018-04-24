f6b3543ac7692c3d608f2cd48152c7e1ec119e51
aj.jammy <aj.jammy09@gmail.com>
2018-04-24 17:34:13 +0700

    [Edited] แก้ไขฟังก์ชั่นการ link ไปหน้า cart โดยส่ง cart id ไปด้วย

--- src/ui/assets/js/core.js
+++ src/ui/assets/js/core.js
@@ -1,6 +1,6 @@
 //Frontend
 const getFrontendURL = (path = '') => `http://${location.host}/${path}`
 const redirectToIndex = () => location.href = getFrontendURL();
-const redirectToCart = () => location.href = getFrontendURL('cart.html');
+const redirectToCart = (id = '') => location.href = getFrontendURL(`cart.html?id=${id}`);
 //Backend
 const getApiURL = (path = '') => `http://${location.host}:5001/${path}`

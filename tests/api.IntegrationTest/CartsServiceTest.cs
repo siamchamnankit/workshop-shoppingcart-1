b2d3d266edc14c8eeee633bd0bd7b810ddafe5a7
iFew <chitpong@gmail.com>
2018-04-25 16:37:36 +0700

    [Edited] เพิ่มการทดสอบ integration test ของการแสดง cart detail

--- tests/api.IntegrationTest/CartsServiceTest.cs
+++ tests/api.IntegrationTest/CartsServiceTest.cs
@@ -65,6 +65,52 @@ namespace api.IntegrationTest
             Assert.Equal((Decimal)119.95, actualResult.subtotal);
             Assert.Equal((Decimal)169.95, actualResult.total);
         }
+
+        [Fact]
+        public void When_Get_Cart_By_Id_1_And_Should_Be_Get_This_Cart_Detail()
+        {
+            var _cartOptions = new DbContextOptionsBuilder<CartsContext>().UseInMemoryDatabase("carts_detail").Options;
+            var _cartContext = new CartsContext(_cartOptions);
+
+            var _productOptions = new DbContextOptionsBuilder<ProductsContext>().UseInMemoryDatabase("carts_products_detail").Options;
+            var _productContext = new ProductsContext(_productOptions);
+
+            CartsModel cartModel = new CartsModel{
+                id = 1,
+                userId = 1,
+                subtotal = (Decimal)12.95,
+                shippingFee = (Decimal)50.00,
+                total = (Decimal)62.95
+            };
+            _cartContext.Carts.Add(cartModel);
+
+            CartProductsModel cartProduct = new CartProductsModel{
+                id = 1,
+                productId = 2,
+                cartId = 1,
+                quantity = 1,
+            };
+            _cartContext.CartProducts.Add(cartProduct);
+            _cartContext.SaveChanges();
+
+            ProductsModel productData = new ProductsModel{
+                id = 2,
+                name = "43 Piece dinner Set",
+                price = (Decimal)12.95,
+                availability = "InStock",
+                stockAvailability = 10,
+                age = "3_to_5",
+                gender = "FEMALE",
+                brand = "CoolKidz"
+            };
+            _productContext.Products.Add(productData);
+            _productContext.SaveChanges();
+
+            CartsService cartsService = new CartsService(_cartContext, _productContext);
+            var actualResult = cartsService.getCart(1, 1);
+
+            Assert.Equal(cartModel, actualResult);
+        }
     }
 }
 

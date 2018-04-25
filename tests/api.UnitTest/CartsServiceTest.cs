c582432a81df9fb36c3b5b44bb03ea715c0b11bc
nathawat <n.thumthiwong@gmail.com>
2018-04-25 16:30:42 +0700

    [Added] เพิ่มกรณีสั่งสินค้า 2 รายการจำนวน 2 ชิ้น

--- tests/api.UnitTest/CartsServiceTest.cs
+++ tests/api.UnitTest/CartsServiceTest.cs
@@ -139,6 +139,36 @@ namespace api.IntegrationTest
             Assert.Equal(25.90M, actualResult.subtotal);
             Assert.Equal(75.90M, actualResult.total);
         }
+
+        [Fact]
+        public void When_Calculate_Cart_With_Two_Products_Price_12_95_And_24_95_With_2_Items_Should_Be_Total_equal_125_80()
+        {
+            var _cartOptions = new DbContextOptionsBuilder<CartsContext>().UseInMemoryDatabase("carts").Options;
+            var _cartContext = new CartsContext(_cartOptions);
+
+            var _productOptions = new DbContextOptionsBuilder<ProductsContext>().UseInMemoryDatabase("products").Options;
+            var _productContext = new ProductsContext(_productOptions);
+            CartsModel cartModel = new CartsModel{
+                userId = 1,
+                createDatetime = DateTime.Now,
+                updateDatetime = DateTime.Now
+            };
+            List<ProductInCartModel> productsInCart = new List<ProductInCartModel>();
+            productsInCart.Add(new ProductInCartModel{
+                price = 12.95M,
+                quantity = 2
+            });
+            productsInCart.Add(new ProductInCartModel{
+                price = 24.95M,
+                quantity = 2
+            });
+
+            CartsService cartsService = new CartsService(_cartContext, _productContext);
+            var actualResult = cartsService.calculate(cartModel, productsInCart);
+    
+            Assert.Equal(75.8M, actualResult.subtotal);
+            Assert.Equal(125.80M, actualResult.total);
+        }
     }
 }
 

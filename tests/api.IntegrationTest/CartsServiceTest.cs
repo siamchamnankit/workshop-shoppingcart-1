2c9dd83ec4ea9399810b36d634847629649bd3f0
iFew <chitpong@gmail.com>
2018-04-25 13:44:14 +0700

    [Created] ทดสอบ การสร้างตะกร้าสินค้า พร้อมสินค้า id2  จำนวน 1 ชิ้น

---
+++ tests/api.IntegrationTest/CartsServiceTest.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading.Tasks;
+using api.IntegrationTest.Fixtures;
+using api.Models;
+using api.Services;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace api.IntegrationTest
+{
+    public class CartsTest
+    {
+        [Fact]
+        public void When_Create_New_Cart_With_Product_ID2_And_Quantity2_ANd_Should_Be_Get_CartID1()
+        {
+            var _cartOptions = new DbContextOptionsBuilder<CartsContext>().UseInMemoryDatabase("carts").Options;
+            var _cartContext = new CartsContext(_cartOptions);
+
+            var _productOptions = new DbContextOptionsBuilder<ProductsContext>().UseInMemoryDatabase("products").Options;
+            var _productContext = new ProductsContext(_productOptions);
+
+            ProductsModel productsData = new ProductsModel{
+                    id = 2,
+                    name = "43 Piece dinner Set",
+                    price = (Decimal)12.95,
+                    availability = "InStock",
+                    stockAvailability = 10,
+                    age = "3_to_5",
+                    gender = "FEMALE",
+                    brand = "CoolKidz"
+                };
+            _productContext.Products.Add(productsData);
+
+            CartsService cartsService = new CartsService(_cartContext, _productContext);
+            var actualResult = cartsService.add(productsData, 1);
+
+            Assert.Equal(1, actualResult.id);
+        }
+    }
+}
+

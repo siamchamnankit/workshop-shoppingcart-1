1b595f1c6eaf20347f5557791e091b79716f8b82
nathawat <n.thumthiwong@gmail.com>
2018-04-25 16:02:21 +0700

    [Added] เพิ่ม test สำหรับสินค้าฟรีเสียแค่ค่าส่ง

--- tests/api.UnitTest/CartsServiceTest.cs
+++ tests/api.UnitTest/CartsServiceTest.cs
@@ -36,6 +36,32 @@ namespace api.IntegrationTest
             Assert.Equal(119.95M, actualResult.subtotal);
             Assert.Equal(169.95M, actualResult.total);
         }
+
+        [Fact]
+        public void When_Calculate_Cart_With_One_Product_Price_0_Should_Be_Total_equal_50()
+        {
+            var _cartOptions = new DbContextOptionsBuilder<CartsContext>().UseInMemoryDatabase("carts").Options;
+            var _cartContext = new CartsContext(_cartOptions);
+
+            var _productOptions = new DbContextOptionsBuilder<ProductsContext>().UseInMemoryDatabase("products").Options;
+            var _productContext = new ProductsContext(_productOptions);
+            CartsModel cartModel = new CartsModel{
+                userId = 1,
+                createDatetime = DateTime.Now,
+                updateDatetime = DateTime.Now
+            };
+            List<ProductInCartModel> productsInCart = new List<ProductInCartModel>();
+            productsInCart.Add(new ProductInCartModel{
+                price = 0.00M,
+                quantity = 1
+            });
+
+            CartsService cartsService = new CartsService(_cartContext, _productContext);
+            var actualResult = cartsService.calculate(cartModel, productsInCart);
+
+            Assert.Equal(0.00M, actualResult.subtotal);
+            Assert.Equal(50.00M, actualResult.total);
+        }
     }
 }
 

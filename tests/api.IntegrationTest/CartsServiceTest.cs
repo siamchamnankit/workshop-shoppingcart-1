814ea18e553085d42da95b807087cbda7d6b877f
karan sivarat <bomb0069@gmail.com>
2019-02-07 15:03:04 +0700

    [Refactor] Integrate Test - Remove duplication test and extract method and variables

--- tests/api.IntegrationTest/CartsServiceTest.cs
+++ tests/api.IntegrationTest/CartsServiceTest.cs
@@ -10,37 +10,53 @@ using Xunit;
 
 namespace api.IntegrationTest
 {
-    public class CartsTest
+    public class DefaultProductDatabaseFixture : AbstractDatabaseFixture
+    {
+        public override string GetDatabaseName() => "DefaultProductDatabaseCartsTest";
+    }
+    public class CartsServiceTest : IClassFixture<DefaultProductDatabaseFixture>
     {
         private const int DEFAULT_USER_ID = 1; 
+        private ProductsContext _productContext;
+        private DefaultProductDatabaseFixture _fixture;
+
+        public CartsServiceTest(DefaultProductDatabaseFixture fixture)
+        {
+            _fixture = fixture;
+            _productContext = fixture.productContext;
+        }
 
         [Fact]
-        public void When_Create_New_Cart_With_43_Piece_Dinner_Set_With_1_Qty_Should_Return_Cart_Detail_With_43_Piece_Dinner_Set () 
+        public void When_Create_New_Cart_With_43_Piece_Dinner_Set_With_1_Qty_Should_Return_Cart_Detail_With_43_Piece_Dinner_Set ()
         {
-            var _cartOptions = new DbContextOptionsBuilder<CartsContext>().UseInMemoryDatabase("create_new_cart_carts2").Options;
-            var _cartContext = new CartsContext(_cartOptions);
-
-            var _productOptions = new DbContextOptionsBuilder<ProductsContext>().UseInMemoryDatabase("create_new_cart_products2").Options;
-            var _productContext = new ProductsContext(_productOptions);
-
-            ProductsModel productsData = new ProductsModel{
-                    id = 2,
-                    name = "43 Piece dinner Set",
-                    price = (Decimal)12.95,
-                    availability = "InStock",
-                    stockAvailability = 10,
-                    age = "3_to_5",
-                    gender = "FEMALE",
-                    brand = "CoolKidz"
-                };
-            _productContext.Products.Add(productsData);
-            _productContext.SaveChanges();
+            ProductsModel adding_product = _fixture._2_43_Piece_dinner_Set;
+            int adding_quantity = 1;
+            CartsContext _cartContext = createCartsContext("create_new_cart_carts2");
+            CartsModel expectedCart = createCartInfo(adding_product, adding_quantity);
 
             CartsService cartService = new CartsService(_cartContext, _productContext);
-
-            AddCartOutputModel cartOutput = cartService.add(productsData, DEFAULT_USER_ID);
+            AddCartOutputModel cartOutput = cartService.add(adding_product, adding_quantity);
             CartsModel actualCart = cartService.getCart(cartOutput.id, DEFAULT_USER_ID);
-            CartsModel expectedCart = new CartsModel{
+
+            Assert.Equal(expectedCart.CartProducts, actualCart.CartProducts);
+            Assert.Equal(expectedCart.userId, actualCart.userId);
+            Assert.Equal(expectedCart.total, actualCart.total);
+            Assert.Equal(expectedCart.subtotal, actualCart.subtotal);
+            Assert.Equal(expectedCart.shippingFee, actualCart.shippingFee);
+            Assert.Equal(expectedCart.shippingMethod, actualCart.shippingMethod);
+        }
+
+        private static CartsContext createCartsContext(string databaseName)
+        {
+            var _cartOptions = new DbContextOptionsBuilder<CartsContext>().UseInMemoryDatabase(databaseName).Options;
+            var _cartContext = new CartsContext(_cartOptions);
+            return _cartContext;
+        }
+
+        private CartsModel createCartInfo(ProductsModel adding_product, int adding_quantity)
+        {
+            CartsModel expectedCart = new CartsModel
+            {
                 userId = DEFAULT_USER_ID,
                 total = 62.95M,
                 subtotal = 12.95M,
@@ -51,138 +67,23 @@ namespace api.IntegrationTest
             };
 
             List<ProductInCartModel> productsInCart = new List<ProductInCartModel>();
-            productsInCart.Add(new ProductInCartModel{
-                    id = 2,
-                    name = "43 Piece dinner Set",
-                    price = (Decimal)12.95,
-                    availability = "InStock",
-                    stockAvailability = 10,
-                    age = "3_to_5",
-                    gender = "FEMALE",
-                    brand = "CoolKidz",
-                    quantity = 1
-                });
-
-                expectedCart.CartProducts = productsInCart;
-
-
-            Assert.Equal(expectedCart.CartProducts, actualCart.CartProducts);
-            Assert.Equal(expectedCart.userId, actualCart.userId);
-            Assert.Equal(expectedCart.total, actualCart.total);
-            Assert.Equal(expectedCart.subtotal, actualCart.subtotal);
-            Assert.Equal(expectedCart.shippingFee, actualCart.shippingFee);
-            Assert.Equal(expectedCart.shippingMethod, actualCart.shippingMethod);
-        }
-
-
-        [Fact]
-        public void When_Create_New_Cart_With_Product_ID2_And_Quantity2_And_Should_Be_Get_CartID1()
-        {
-            var _cartOptions = new DbContextOptionsBuilder<CartsContext>().UseInMemoryDatabase("create_new_cart_carts").Options;
-            var _cartContext = new CartsContext(_cartOptions);
-
-            var _productOptions = new DbContextOptionsBuilder<ProductsContext>().UseInMemoryDatabase("create_new_cart_products").Options;
-            var _productContext = new ProductsContext(_productOptions);
-
-            ProductsModel productsData = new ProductsModel{
-                    id = 2,
-                    name = "43 Piece dinner Set",
-                    price = (Decimal)12.95,
-                    availability = "InStock",
-                    stockAvailability = 10,
-                    age = "3_to_5",
-                    gender = "FEMALE",
-                    brand = "CoolKidz"
-                };
-            _productContext.Products.Add(productsData);
-            _productContext.SaveChanges();
-
-            CartsService cartsService = new CartsService(_cartContext, _productContext);
-            var actualResult = cartsService.add(productsData, 1);
-
-            Assert.Equal(1, actualResult.id);
-        }
-
-        [Fact]
-        public void When_Calculate_Cart_With_One_Product_Price_119_95_Should_Be_Total_equal_169_95()
-        {
-            var _cartOptions = new DbContextOptionsBuilder<CartsContext>().UseInMemoryDatabase("calculate_carts").Options;
-            var _cartContext = new CartsContext(_cartOptions);
-
-            var _productOptions = new DbContextOptionsBuilder<ProductsContext>().UseInMemoryDatabase("calculate_products").Options;
-            var _productContext = new ProductsContext(_productOptions);
-            CartsModel cartModel = new CartsModel{
-                userId = 1,
-                createDatetime = DateTime.Now,
-                updateDatetime = DateTime.Now
-            };
-            List<ProductInCartModel> productsInCart = new List<ProductInCartModel>();
-            productsInCart.Add(new ProductInCartModel{
-                price = (Decimal)119.95,
-                quantity = 1
-            });
-
-            CartsService cartsService = new CartsService(_cartContext, _productContext);
-            var actualResult = cartsService.calculate(cartModel, productsInCart);
-
-            Assert.Equal((Decimal)119.95, actualResult.subtotal);
-            Assert.Equal((Decimal)169.95, actualResult.total);
-        }
-
-        [Fact]
-        public void When_Get_Cart_By_Id_1_And_Should_Be_Get_This_Cart_Detail()
-        {
-            var _cartOptions = new DbContextOptionsBuilder<CartsContext>().UseInMemoryDatabase("get_carts_carts_detail").Options;
-            var _cartContext = new CartsContext(_cartOptions);
-
-            var _productOptions = new DbContextOptionsBuilder<ProductsContext>().UseInMemoryDatabase("get_carts_carts_products_detail").Options;
-            var _productContext = new ProductsContext(_productOptions);
-
-            CartsModel cartModel = new CartsModel{
-                id = 1,
-                userId = 1,
-                subtotal = (Decimal)12.95,
-                shippingFee = (Decimal)50.00,
-                total = (Decimal)62.95
-            };
-            _cartContext.Carts.Add(cartModel);
-
-            CartProductsModel cartProduct = new CartProductsModel{
-                id = 1,
-                productId = 2,
-                cartId = 1,
-                quantity = 1,
-            };
-            _cartContext.CartProducts.Add(cartProduct);
-            _cartContext.SaveChanges();
-
-            ProductsModel productData = new ProductsModel{
-                id = 2,
-                name = "43 Piece dinner Set",
-                price = (Decimal)12.95,
+            productsInCart.Add(new ProductInCartModel
+            {
+                id = adding_product.id,
+                name = adding_product.name,
+                price = adding_product.price,
                 availability = "InStock",
                 stockAvailability = 10,
-                age = "3_to_5",
-                gender = "FEMALE",
-                brand = "CoolKidz"
-            };
-            _productContext.Products.Add(productData);
-            _productContext.SaveChanges();
+                age = adding_product.age,
+                gender = adding_product.gender,
+                brand = adding_product.brand,
+                quantity = adding_quantity
+            });
 
-            CartsService cartsService = new CartsService(_cartContext, _productContext);
-            var actualResult = cartsService.getCart(1, 1);
-
-            Assert.Equal(cartModel, actualResult);
-        }
-        [Fact]
-        public void AddCartInputModel_should_be_correctly(){
-            AddCartInputModel input = new AddCartInputModel{
-                id = 1,
-                quantity = 10
-            };
-            Assert.Equal(1, input.id);
-            Assert.Equal(10, input.quantity);
+            expectedCart.CartProducts = productsInCart;
+            return expectedCart;
         }
     }
+
 }
 

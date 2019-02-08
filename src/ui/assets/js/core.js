1389cebe4a9ad0e2f0bff9e94fe3c5fcc36e45d3
nathawat <n.thumthiwong@gmail.com>
2019-02-08 10:45:20 +0700

    [Update] Change function get Order summary

--- src/ui/assets/js/core.js
+++ src/ui/assets/js/core.js
@@ -2,6 +2,6 @@
 const getFrontendURL = (path = '') => `http://${location.host}/${path}`
 const redirectToIndex = () => location.href = getFrontendURL();
 const redirectToCart = (id = '') => location.href = getFrontendURL(`cart.html?id=${id}`);
-const redirectToOrderSummary = () => location.href = getFrontendURL('order_detail.html');
+const redirectToOrderSummary = (id = '') => location.href = getFrontendURL(`order_detail.html?id=${id}`);
 //Backend
 const getApiURL = (path = '') => `http://${location.host}:5001/${path}`
